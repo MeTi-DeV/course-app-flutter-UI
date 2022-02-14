237b106364730f5665c60c9dab646afb9c697e7c
MeTi-DeV <mahdi_zarei78@yahoo.com>
2022-02-14 20:31:11 +0330

    start project

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kBackground = Color(0xFFFCFCFC);
+const kPrimaryLight = Color(0xFFEDF6F3);
+const kPrimary = Color(0xFFDAEFE8);
+const kPrimaryDark = Color(0xFF88A1AE);
+const kAccent = Color(0xFFFDCA73);
+const kFont = Color(0xFF252435);
+const kFontLight = Color(0xFFB4B7BF);
